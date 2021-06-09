e6e9c5686d87bdccaa08859d3586ca8ef723073d
Luciano Manerich Junior <lucianojr@gmail.com>
2021-06-09 12:35:03 -0300

    Initial project commit

---
+++ script/popup.js
@@ -0,0 +1,45 @@
+var WHATSAPP_URL = 'https://wa.me/';
+
+startMessage.addEventListener('click', async () => {
+    await sendMessage();
+});
+
+async function sendMessage() {
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    if (phoneNumber.lenght < 10) {
+        console.log('min phone');
+        alert('Informe o DDD');
+        return;
+    }
+
+    if (phoneNumber.indexOf("55") !== 0) {
+        phoneNumber = "55" + phoneNumber;
+    }
+
+    phoneNumber = phoneNumber.replace(/\D/g,'');
+
+    var tab = await openWhatsAppTab();
+    console.log(tab);
+    await chrome.tabs.update(tab.id, { url: WHATSAPP_URL + phoneNumber });
+}
+
+async function openWhatsAppTab() {
+    return chrome.tabs
+        .query({ url: '*://web.whatsapp.com/*' })
+        .then((tabs) => {
+            console.log('tab query');
+            console.log(tabs);
+            if (tabs.length === 1) {
+                return tabs[0];
+            }
+
+            return chrome.tabs.create({});
+        })
+        .then((tab) => {
+            console.log('tab created');
+            console.log(tab);
+            if (!tab) return;
+
+            return tab;
+        });
+}
